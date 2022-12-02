d1ddfacc0c90edfaaaba73922e27ce10b07987cb
joakG16 <4379@holbertonschool.com>
2022-12-02 15:35:05 -0300

    task 1 done

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your
+Python script to export data in the CSV format.
+API used: https://jsonplaceholder.typicode.com/
+"""
+
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    num = argv[1]  # remember python3 does not count as argument
+
+    user_query = {'id': num}  # this is added as query parameter
+    # that are appended to the endpoint URL
+    response_1 = requests.get("https://jsonplaceholder.typicode.com/users",
+                              params=user_query)  # endpoint URL
+
+    todo_query = {'userId': num}  # match todo list with user specified
+    response_2 = requests.get("https://jsonplaceholder.typicode.com/todos",
+                              params=todo_query)
+
+    user = response_1.json()  # .json() is a builtin JSON decoder
+    # from request module, returning a list of dictionaries in this case
+    username = user[0].get('username')
+
+    todo_list = response_2.json()
+
+    with open('USER_ID.csv', mode='w') as employee_file:
+        ''' creating writer oject to convert my data into a delimeted string'''
+        ''' csv.QUOTE_ALL - Specifies the writer object to write CSV
+        file with quotes around all the entries, non and numeric ones'''
+        row_writer = csv.writer(employee_file, quoting=csv.QUOTE_ALL,
+                                delimiter=',')
+
+        for task in todo_list:
+            ''' converting data and then writing it into the .csv file '''
+            row_writer.writerow((task.get('userId'), username,
+                                 task.get('completed'), task.get('title')))
