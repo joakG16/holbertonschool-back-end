c2d04f914175e60e8d69c66f7f2727f40ace1ad3
joakG16 <4379@holbertonschool.com>
2022-12-04 11:58:32 -0300

    changing username extract

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -17,10 +17,9 @@ if __name__ == '__main__':
     todo_list = response_2.json()
 
     uid_filename = f'{num}.csv'
-    username = user[0].get('username')
     with open(uid_filename, mode='w') as employee_file:
         row_writer = csv.writer(employee_file, quoting=csv.QUOTE_ALL,
                                 delimiter=',')
         for task in todo_list:
-            row_writer.writerow((task.get('userId'), username,
+            row_writer.writerow((task.get('userId'), user[0].get('username'),
                                  task.get('completed'), task.get('title')))
