21efeb07427506cdb0ac8644ae1b18ab0690dc1d
joakG16 <4379@holbertonschool.com>
2022-12-04 12:12:16 -0300

    task 2 different approach

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,46 +1,22 @@
 #!/usr/bin/python3
-"""
-Using what you did in the task #0, extend your
-Python script to export user's data in the JSON format.
-API used: https://jsonplaceholder.typicode.com/
-"""
+""" extend your Python script to export data in the CSV format """
 
 
-if __name__ == "__main__":
-    import json
+if __name__ == '__main__':
     import requests
     from sys import argv
 
-    id_num = argv[1]  # remember python3 does not count as argument
-
-    ''' extracting desired user/employee '''
-    user_query = {'id': id_num}  # this is added as query parameter
-    # that are appended to the endpoint URL
-    response_1 = requests.get("https://jsonplaceholder.typicode.com/users",
-                              params=user_query)  # endpoint URL
-
-    ''' extracting desired user's TODO list'''
-    todo_query = {'userId': id_num}  # match todo list with user specified
-    response_2 = requests.get("https://jsonplaceholder.typicode.com/todos",
-                              params=todo_query)
-
-    user = response_1.json()  # .json() is a builtin JSON decoder
-    # from request module, returning a list of dictionaries in this case
-    user_todo_list = response_2.json()
-
-    username = user[0].get('username')
-    ''' creating list of dictionaries (user's tasks) (list comprehension)'''
-    task = [{"task": task.get('title'), "username": username,
-            "completed": task.get('completed')} for task in user_todo_list]
-
-    # creating dictionary with user as key and respective tasks as its values
-    user_todo_dict = {}
-    user_todo_dict[id_num] = task
-
-    # Serializing json
-    json_object = json.dumps(user_todo_dict)
-
-    uid_filename = f'{id_num}.json'
-    # Writing to json file
-    with open(uid_filename, "w") as jsonfile:
-        jsonfile.write(json_object)
+    u_id = argv[1]
+    api_url = "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
+    api_url2 = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(u_id)
+    response = requests.get(api_url).json()
+    EMPLOYEE_NAME = response.get('username')
+    response = requests.get(api_url2).json()
+    f_name = u_id + '.csv'
+    with open(f_name, 'w', encoding='utf-8') as f:
+        for info in response:
+            TASK_COMPLETED_STATUS = info.get("completed")
+            TASK_TITLE = info.get("title")
+            f.write('"{}","{}","{}","{}"\n'.format(
+                u_id, EMPLOYEE_NAME, TASK_COMPLETED_STATUS, TASK_TITLE))
